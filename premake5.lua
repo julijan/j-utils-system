6485f06bb81175d28315cfaf0e1d85fa67685049
Julijan Andjelic <julijan.andjelic@gmail.com>
2025-10-12 14:01:13 +0200

    initialized cppm package

---
+++ premake5.lua
@@ -0,0 +1,21 @@
+workspace "j-utils-system"
+	configurations { "Debug", "Release" }
+
+project "j-utils-system"
+	language "C++"
+	kind "StaticLib"
+	cppdialect "C++20"
+	architecture "x64"
+	targetdir "bin/%{cfg.buildcfg}"
+	files { "./src/**.h", "./src/**.cpp" }
+	includedirs { "./includes/src", "./includes/src/**", "./includes/uses/src/**" }
+	libdirs { "./includes/lib/**", "./includes/uses/lib/**" }
+	links {
+		
+	}
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		symbols "On"
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		optimize "On"
